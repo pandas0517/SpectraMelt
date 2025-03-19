1df77a94756eae3ea0535c4c428519aba0d10278
Peter Swartz <pandas0629@yahoo.com>
2025-03-19 19:41:12 -0400

    First use of .env and main

--- src/main.py
+++ src/main.py
@@ -1,19 +1,15 @@
 '''
-Created on Jul 10, 2024
-
 @author: pete
 '''
 if __name__ == '__main__':
     import json
     import os
     from pathlib import Path
-    import matplotlib.pyplot as plt
-    import numpy as np
-    from queue import Queue
-    # from signals import simulate_system
-    from scipy.fft import fft, ifft
-    from math import pi
-    from decoder import get_all_file_paths, get_all_sub_dirs, create_nyfr_output
-    from decoder import batch_recover, create_dictionaries, create_mlp1_models
-    from decoder import get_all_file_names, meta_input_output, analyze_dfs
-    from OMP import OMP
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    with open(Path(os.getenv('SYSTEM_CONF')), 'r') as file:
+        system_conf = json.load(file)
+    with open(Path(os.getenv('FILEPATH_CONF')), 'r') as file:
+        filepath_conf = json.load(file)
+    pass
