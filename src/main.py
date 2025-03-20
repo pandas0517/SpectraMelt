528f100bf396c357cefecba413b2588573c2249d
Peter Swartz <pandas0629@yahoo.com>
2025-03-20 12:06:51 -0400

    Updating create_real_time NYFR function

--- src/main.py
+++ src/main.py
@@ -2,16 +2,14 @@
 @author: pete
 '''
 if __name__ == '__main__':
-    import json
     import os
     from pathlib import Path
     from dotenv import load_dotenv
     from NYFR import NYFR
 
     load_dotenv()
-    with open(Path(os.getenv('SYSTEM_CONF')), 'r') as file:
-        system_conf = json.load(file)
+
     with open(Path(os.getenv('FILEPATH_CONF')), 'r') as file:
         filepath_conf = json.load(file)
-    nyfr = NYFR(system_conf)
+    nyfr = NYFR(Path(os.getenv('SYSTEM_CONF')))
     pass
---
+++ src/utility.py
@@ -0,0 +1,18 @@
+import json
+import sys
+
+def load_settings(self, file_path):
+    print("Loading Settings from file: {}", file_path)
+    try:
+        with open(file_path, 'r') as file:
+            system_config = json.load(file)
+            return system_config
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+        sys.exit(1)
+    except json.JSONDecodeError:
+        print("Error decoding JSON. Please check the file format.")
+        sys.exit(1)
+    except IsADirectoryError:
+        print("Invalid directory. Please check the file path.")
+        sys.exit(1)
